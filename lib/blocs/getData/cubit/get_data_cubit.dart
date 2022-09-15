3152112115306d3cd183810c9898e4d140fa6367
zanuar35 <rahmatzanuar35@gmail.com>
2022-09-15 14:30:55 +0700

    delete print statement

--- lib/blocs/getData/cubit/get_data_cubit.dart
+++ lib/blocs/getData/cubit/get_data_cubit.dart
@@ -1,6 +1,7 @@
 // ignore: depend_on_referenced_packages
 import 'dart:convert';
 
+// ignore: depend_on_referenced_packages
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:http/http.dart' as http;
@@ -13,7 +14,7 @@ class GetDataCubit extends Cubit<GetDataState> {
 
   void getData() async {
     emit(GetDataLoading());
-    print("Loading data...");
+
     var res = await http.get(Uri.parse(
         "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=2b7494b2a0754ebcb758148be4e32842"));
 
@@ -23,12 +24,10 @@ class GetDataCubit extends Cubit<GetDataState> {
       NewsModel newsModel = NewsModel.fromJson(dataMap);
       List<Articles>? newsData = newsModel.articles;
       emit(GetDataSuccess(newsData));
-      print(newsData);
-      print("Success");
     }
     // jika gagal get data
     else if (res.statusCode != 200) {
-      print("Error: " + res.statusCode.toString());
+      //
     }
   }
 }
